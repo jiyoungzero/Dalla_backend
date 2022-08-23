e68a06f0d7b6566c9844eb6b706a0e74fc41122b
이지영 <wldudwldhd@naver.com>
2022-08-23 16:34:28 +0900

    add IsAuthenticated to crud

--- restcrud/settings.py
+++ restcrud/settings.py
@@ -49,6 +49,10 @@ REST_FRAMEWORK = {
         # "accounts.authentications.JWTAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    "DEFAULT_PERMISSION_CLASSES" : [
+    "rest_framework.permissions.IsAuthenticated",
+    ],
+    
 }
 
 MIDDLEWARE = [
