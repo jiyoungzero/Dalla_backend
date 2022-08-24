65789ebd1cc7f97903581ab8054bdbf05289e99e
이지영 <wldudwldhd@naver.com>
2022-08-24 23:24:20 +0900

    .

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,6 @@ from accounts.views import *
 urlpatterns = [
     path('regist', user_regist, name="regist"),
     path('login', user_login, name="login"),
-    
     # path('test', test, name="test"),
     # path('login2', TokenObtainPairView.as_view(), name="login2"),
     # path('refresh', TokenRefreshView.as_view(), name="refresh"),
